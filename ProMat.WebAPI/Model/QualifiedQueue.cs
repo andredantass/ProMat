065409d39e15cc8ec4cb11c0f2206896e0619380
André Dantas <a.dantas@intcom.com.br>
2020-10-31 04:05:24 -0300

    Adding new code

--- ProMat.WebAPI/Model/QualifiedQueue.cs
+++ ProMat.WebAPI/Model/QualifiedQueue.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace ProMat.WebAPI.Model
 {
     public class QualifiedQueue
     {
-        
+        public int QualifiedQueueId { get; set; }
+        public string FirstName { get; set; }
+        public string Phone { get; set; }
+        public string Situation { get; set; }
+        public DateTime DateBorn { get; set; }
+        public string PrevSituation { get; set; }
+        public DateTime DateJobEnd { get; set; }
+
     }
 }
