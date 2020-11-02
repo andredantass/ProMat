1c077a4f01eb1b4708dd10fb6dafb0466cc7d09f
André Dantas <a.dantas@intcom.com.br>
2020-11-02 02:32:35 -0300

    Servidor Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
+  timeoutSessionIdle: 900,
+  apiURL: 'http://localhost:5000/api',
+  appInsights: {
+      instrumentationKey: '1ffaed42-6265-458a-ae0d-ffd0600ac948'
+  },
+  SSO_BASE_URL: 'https://ssohmo.blueopex.com',
+  auth: {
+      ROOT_URL: "https://localhost:5000/app/auth",
+      AUTH_URL: 'https://ssohmo.blueopex.com/',
+      AUTH_CLINTID: 'bolight.azurewebsites.net',
+      REQUERID_SCOPE: 'openid profile offline_access',
+      LOGOUT_URL: 'https://ssohmo.blueopex.com/Account/logout',
+
+      RESPONSE_TYPE: 'token id_token',
+      REQUEST_CLAIMS: ['company']
+  }, 
   production: true
 };
