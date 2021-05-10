4f317faed3473faf3a9856e91f6946667eb7dc9b
andredantascontrolid <a.dantas@intcom.com.br>
2021-05-10 00:12:35 -0300

    Adding new form answer

--- ProMat.FrontEnd/src/app/forms/forms.component.ts
+++ ProMat.FrontEnd/src/app/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,AfterViewChecked, AfterViewInit } from '@angular/core';
+import { Component,ElementRef, OnInit,AfterViewChecked, AfterViewInit,ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormService } from '../services/form.service';
 
@@ -11,13 +11,30 @@ declare var startInitial: Function;
 export class FormsComponent implements OnInit,  AfterViewChecked, AfterViewInit {
   ipAddress: string;
   location: string;
+  autoSaveInterval;
+  @ViewChild('devQuestionChildWasBorn', { static: true }) dvQChildWasBorn: ElementRef;
+  @ViewChild('dvQuestionChildLess5Years',{ static:true }) dvQuestionChildLess5Years : ElementRef;
+  @ViewChild('dvQuestionWorkBeforeBorn', {static: true}) dvQuestionWorkBeforeBorn :ElementRef; 
+  @ViewChild('dvQuestionInsurances', {static: true}) dvQuestionInsurances :ElementRef; 
+  @ViewChild('dvCongratulation', {static: true}) dvCongratulation :ElementRef; 
+  @ViewChild('checkMarkAnswer',{static:true}) checkMarkAnswer :ElementRef;
+
+  
+  
   constructor(private router: Router, private formService: FormService) { }
 
   RedirectQualification(status)
   {
     this.router.navigate(['/qualification']);
   }
+
+
+  	/* setTimeout(() => {
+		document.getElementById('devQuestionChildWasBorn').style.display = 'block';
+	}, tempo) */
+
   ngOnInit() {
+
     startInitial();
     this.getIP();
   }
@@ -46,9 +63,76 @@ export class FormsComponent implements OnInit,  AfterViewChecked, AfterViewInit 
   ngAfterViewChecked() {
 
   }
+  answerChildWasBorn()
+  {
+    this.dvQChildWasBorn.nativeElement.style.display = 'none';
+    
+    this.checkMarkAnswer.nativeElement.style.display = 'block';
+    this.autoSaveInterval = setInterval(() => {
+      this.checkMarkAnswer.nativeElement.style.display = 'none';
+      this.dvQuestionChildLess5Years.nativeElement.style.display = "block";
+    }, 2000);
+
+  }
+
+  answerChildLess5Years()
+  {
+    this.ngOnDestroy();
+    this.dvQuestionChildLess5Years.nativeElement.style.display = "none";
+
+      this.checkMarkAnswer.nativeElement.style.display = 'block';
+      this.autoSaveInterval = setInterval(() => {
+        this.checkMarkAnswer.nativeElement.style.display = 'none';
+        this.dvQuestionWorkBeforeBorn.nativeElement.style.display = "block";
+        
+      }, 2000);
+
+  }
+  answerWorkBeforeBorn()
+  {
+    this.ngOnDestroy();
+
+    this.dvQuestionWorkBeforeBorn.nativeElement.style.display = "none";
+
+    this.checkMarkAnswer.nativeElement.style.display = 'block';
+    this.autoSaveInterval = setInterval(() => {
+      this.checkMarkAnswer.nativeElement.style.display = 'none';
+      this.dvQuestionInsurances.nativeElement.style.display = "block";
+      
+    }, 2000);
+
+  }
+  answerWorkInsurances(response)
+  {
+    this.ngOnDestroy();
+    if(response)
+    {
+      this.dvQuestionInsurances.nativeElement.style.display = "none";
+
+      this.checkMarkAnswer.nativeElement.style.display = 'block';
+      this.autoSaveInterval = setInterval(() => {
+        this.checkMarkAnswer.nativeElement.style.display = 'none';
+        this.dvCongratulation.nativeElement.style.display = "block";
+        
+      }, 2000);
+    }
+    else
+    {}
+  }
+  sendToComercial()
+  {
+
+  }
+  ngOnDestroy() {
+    if (this.autoSaveInterval) {
+      clearInterval(this.autoSaveInterval);
+    }
+  }
   countAccessYes()
   {
-    this.formService.accessCount("Sim", this.ipAddress).subscribe();
+    this.dvQChildWasBorn.nativeElement.style.display = 'none';
+    this.dvQuestionChildLess5Years.nativeElement.style.display = "block";
+    // this.formService.accessCount("Sim", this.ipAddress).subscribe();
   }
   countAccessNo()
   {
